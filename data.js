32f61f77ea572b9161b9c8ac07fdce86248cbda2
CodeConscious <hello@codeconscio.us>
2022-06-27 17:44:19 +0900

    Add more data entities

--- data.js
+++ data.js
@@ -4,7 +4,10 @@ exports.personDb = [
     { id: 1, civilianName: 'Scott Summers', superName: 'Cyclops', teamId: 1 },
     { id: 2, civilianName: 'Ororo Munroe', superName: 'Storm', teamId: 1 },
     { id: 3, civilianName: 'Steven Rogers', superName: 'Captain America', teamId: 2 },
-    { id: 4, civilianName: 'Reed Richards', superName: 'Mister Fantastic', teamId: 3 }
+    { id: 4, civilianName: 'Reed Richards', superName: 'Mister Fantastic', teamId: 3 },
+    { id: 5, civilianName: 'T\'Challa', superName: 'Black Panther', teamId: 2 },
+    { id: 6, civilianName: 'Katherine Pryde', superName: 'Shadowcat', teamId: 1 },
+    { id: 7, civilianName: 'Everett Thomas', superName: 'Synch', teamId: 1 }
 ]
 
 exports.teamDb = [
