cbad1955d358cf2c6a3d34954e592772bdea3c60
CodeConscious <hello@codeconscio.us>
2022-06-27 17:35:43 +0900

    Move data to dedicated file

---
+++ data.js
@@ -0,0 +1,14 @@
+/* These arrays represent data that would come from a database in a real application. */
+
+exports.personDb = [
+    { id: 1, civilianName: 'Scott Summers', superName: 'Cyclops', teamId: 1 },
+    { id: 2, civilianName: 'Ororo Munroe', superName: 'Storm', teamId: 1 },
+    { id: 3, civilianName: 'Steven Rogers', superName: 'Captain America', teamId: 2 },
+    { id: 4, civilianName: 'Reed Richards', superName: 'Mister Fantastic', teamId: 3 }
+]
+
+exports.teamDb = [
+    { id: 1, name: 'X-Men' },
+    { id: 2, name: 'Avengers' },
+    { id: 3, name: 'Fantastic Four' }
+]
--- server.js
+++ server.js
@@ -10,19 +10,7 @@ const {
 } = require('graphql')
 const app = express()
 
-// These arrays represent data that would come from the database in a real application.
-const personDb = [
-    { id: 1, civilianName: 'Scott Summers', superName: 'Cyclops', teamId: 1 },
-    { id: 2, civilianName: 'Ororo Munroe', superName: 'Storm', teamId: 1 },
-    { id: 3, civilianName: 'Steven Rogers', superName: 'Captain America', teamId: 2 },
-    { id: 4, civilianName: 'Reed Richards', superName: 'Mister Fantastic', teamId: 3 }
-]
-
-const teamDb = [
-    { id: 1, name: 'X-Men' },
-    { id: 2, name: 'Avengers' },
-    { id: 3, name: 'Fantastic Four' }
-]
+const { personDb, teamDb } = require('./data');
 
 const PersonType = new GraphQLObjectType({
     name: 'Person',
